f2473047f2ecc2eda881c32ec98c4575a4c5e2d8
unknown <mutafaailuun>
2023-08-12 09:10:45 +0700

    deleting admin panel

--- model/c-header.php
+++ model/c-header.php
@@ -5,7 +5,7 @@
             require "controller/config.php";
             $p = mysqli_query($koneksi, "SELECT * FROM tbl_profil WHERE  id_profil='1'");
             while ($data = mysqli_fetch_array($p)) {
-                echo '<a href="/sik/" class="navbar-brand"><img src="images/home.png" height="100%"><b>&nbsp&nbsp ' . $data['nm_aplikasi'] . '</b> </a>';
+                echo '<a href="/" class="navbar-brand"><img src="images/home.png" height="100%"><b>&nbsp&nbsp ' . $data['nm_aplikasi'] . '</b> </a>';
             } ?>
             <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
                 <span class="icon-bar"></span>
@@ -15,8 +15,9 @@
         </div>
         <div class="navbar-collapse collapse" id="navbar-main">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="caridata"><span class='glyphicon glyphicon-briefcase' aria-hidden='true'></span>&nbsp;<b> CEK KELULUSAN</b></a></li>
-                <li><a href="login"><span class='glyphicon glyphicon-lock' aria-hidden='true'></span>&nbsp;<b> LOGIN</b></a></li>
+                <li><a href="caridata"><span class='glyphicon glyphicon-briefcase' aria-hidden='true'></span>&nbsp;<b>
+                            CEK KELULUSAN</b></a></li>
+
             </ul>
         </div>
     </div>
